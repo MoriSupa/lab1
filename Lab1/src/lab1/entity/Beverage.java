8fe2ec47562623040992d37dd2868822eeadd3e9
sansmori <omnibos@163.com>
2016-03-14 21:12:23 +0800

    Add getter and setter and other operations for the beverage class

--- Lab1/src/lab1/entity/Beverage.java
+++ Lab1/src/lab1/entity/Beverage.java
@@ -3,6 +3,9 @@ package lab1.entity;
 import java.util.ArrayList;
 
 public class Beverage {
+
+
+
 	private String base; //name of the beverage
 	private String size; //size of the beverage
 	private String category; //whether it's a coffee or tea in virtue.
@@ -11,8 +14,57 @@ public class Beverage {
 	
 	
 	
+	
+	
+	
 	//this function is for presenting the beverage detail
 	public String getDescription(){
 		return "";
 	}
+	
+	//getter and setter for base
+	public String getBase() {
+		return base;
+	}
+
+	public void setBase(String base) {
+		this.base = base;
+	}
+
+	//getter and setter size
+	public String getSize() {
+		return size;
+	}
+
+	public void setSize(String size) {
+		this.size = size;
+	}
+
+	//getter and setter category
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	//getter for ingredients
+	public ArrayList<String> getIngredients() {
+		return ingredients;
+	}
+
+	//more operations for ingredients
+	
+	//to add an ingredient for a beverage
+	public void addIngredients(String ing){
+		this.ingredients.add(ing);
+	}
+	//to clear all ingredients 
+	public void clearIngredients(String ing){
+		this.ingredients.clear();
+	}
+
+
+
 }
