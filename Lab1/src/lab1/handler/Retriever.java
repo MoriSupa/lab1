228b18d34cbe72be4adf237c9c45923332aaccda
Niu1234 <635449644@qq.com>
2016-03-16 21:37:17 +0800

    tian jia IO

--- Lab1/src/lab1/handler/Retriever.java
+++ Lab1/src/lab1/handler/Retriever.java
@@ -8,13 +8,43 @@ public class Retriever implements I_Retriever {
 	private String url;
 	
 	public Retriever(String _url){
-		this.url=_url;
+		this.url=_url;		
 	}	
 	
 	@Override
 	public JSONObject retrieve() {
 		//use the this.url as file path
-		return null;
+	     //从给定位置获取文件
+        File file = new File(url);
+        BufferedReader reader = null;
+        //返回值,使用StringBuffer
+        StringBuffer data = new StringBuffer();
+        //
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            //每次读取文件的缓存
+            String temp = null;
+            while((temp = reader.readLine()) != null){
+                data.append(temp);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            //关闭文件流
+            if (reader != null){
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        JSONTokener jsonTokener = new JSONTokener(data.toString()); 
+        JSONObject retJSONObject;
+        retJSONObject=(JSONObject)jsonTokener.nextValue();  
+		return retJSONObject;
 	}
 
 }
