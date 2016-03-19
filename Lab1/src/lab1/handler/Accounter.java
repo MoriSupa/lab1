3ad5d5251477dee2535e2e5e8b8a815cf915221e
sansmori <omnibos@163.com>
2016-03-19 09:55:32 +0800

    Accounter's bug has been de-ed. Next Thing to do: A second-time re-factoring.

--- Lab1/src/lab1/handler/Accounter.java
+++ Lab1/src/lab1/handler/Accounter.java
@@ -15,16 +15,47 @@ public class Accounter implements I_Accounter {
 
 	@Override
 	public double compute(Beverage bev) {
+		if(bev==null) return -1; //test whether beverage null
+		
+		
 		double total = 0.0;
 		// add the base value
-
-		JSONObject baseInfo = dic.getJSONObject("basic").getJSONObject(bev.getCategory());
-		JSONObject ingre = dic.getJSONObject("ingredient");
-		total += baseInfo.getJSONObject("kind").getDouble(bev.getBase())
-				+ baseInfo.getJSONObject("size").getDouble(bev.getSize());
-		for (int i = 0; i < bev.getIngredients().size(); i++) {
-			total += ingre.getDouble(bev.getIngredients().get(i));
+		JSONObject baseInfo;
+		
+		
+		
+		
+		
+		if(dic.getJSONObject("basic").has(bev.getCategory())){
+			baseInfo = dic.getJSONObject("basic").getJSONObject(bev.getCategory());
+		}else{
+			return -1;
 		}
+		
+		JSONObject ingre = dic.getJSONObject("ingredient");
+		
+		
+		//some test for illegal input
+		if(baseInfo.getJSONObject("kind").has(bev.getBase())){
+			total+=baseInfo.getJSONObject("kind").getDouble(bev.getBase());
+		}else{
+			return -1;
+		}
+		
+		if(baseInfo.getJSONObject("size").has(bev.getSize())){
+			total+=baseInfo.getJSONObject("size").getDouble(bev.getSize());
+		}else{
+			return -1;
+		}
+		
+		for (int i = 0; i < bev.getIngredients().size(); i++) {
+			if(ingre.has(bev.getIngredients().get(i))){
+				total += ingre.getDouble(bev.getIngredients().get(i));
+			}else{
+				return -1;
+			}
+		}
+		
 		return total;
 	}
 
@@ -37,5 +68,7 @@ public class Accounter implements I_Accounter {
 		}
 		return total;
 	}
+	
+	
 
 }
